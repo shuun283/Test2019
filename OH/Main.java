51504b744f3f98860df9e9c4d282f48a7326ed2b
Wuitar <ppiswinner@gmail.com>
2019-06-16 10:35:01 +0900

    new push

---
+++ OH/Main.java
@@ -0,0 +1,11 @@
+package forth;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TV2 oldFashion = new TV2();
+		oldFashion.replay();
+	}
+
+}
---
+++ OH/TV2.java
@@ -0,0 +1,10 @@
+package forth;
+
+public class TV2 extends TV {
+	@Override
+	public void replay() {
+		// TODO Auto-generated method stub
+		this.color = "BW";
+		super.replay();
+	}
+}
